ef19bbef570d60d75e01a74d9b902c966c974d6d
edman manigat <e.manigat@gmail.com>
2018-11-08 22:58:41 -0400

    Update new work 2/new work 2/main.cpp

---
+++ new work 2/new work 2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"programa ciclo for"<<endl;
+    for (a=1; a<=b; b=100)
+    {
+        cout<<  << endl;
+    }
+    return 0;
+}
