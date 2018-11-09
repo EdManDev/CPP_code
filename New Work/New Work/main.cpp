38f9e4de8d74a1b87e09e0afd2dc5cd4b9bad29e
edman manigat <e.manigat@gmail.com>
2018-11-08 22:57:41 -0400

    Update New Work/New Work/main.cpp

---
+++ New Work/New Work/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<math.h>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Este programa imprime la raiz cuadrado de un numero \n"<<endl;
+    cin>> a ;
+    b=sqrt(a);
+    cout<<"La raiz cuadrada es : "<< b <<endl;
+    return 0;
+}
+
