97e5b2d8adfa30d147c9bf896277a84e6787d5e4
EdManDev <ed.manigat@gmail.com>
2018-11-15 20:53:59 -0400

    Add files via upload

---
+++ plata.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int opciones_1= 100, opciones_2= 200, opciones_3= 500;
+
+    printf("ingrese es el plata: \n");
+    
+    scanf("%d", &opciones_1);
+    
+    if(opciones_1 >= 100)
+    
+        printf("%d El plata es\n", opciones_1);
+        
+    else
+    
+        printf("%d el plato debe mass o igual que: \n",);
+        
+    return 0;
+}
+
---
+++ plato2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int cantitad,  plato , opci=100;
+	
+    printf("\n Enter an integer: ");
+    
+    scanf("%d", &cantitad);
+    
+	if(cantitad > 50)
+	
+	cantitad=plato*75;
+	{	
+	
+	 	printf("les plato de la opcio 100 es: %d.\n", cantitad);
+	 	
+	}
+	
+	return 0;
+	
+}
+
