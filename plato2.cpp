03ff8285053d31b41bbf38c6da48bdea8aeff17b
EdManDev <ed.manigat@gmail.com>
2018-11-22 12:26:15 -0500

    Update plato2.cpp

--- plato2.cpp
+++ plato2.cpp
@@ -6,21 +6,20 @@ using namespace std;
 
 int main()
 {
-	int cantitad,  plato , opci=100;
+	int cantitad,  plato=1 , opci=100;
 	
-    printf("Enter an integer: \n");
+    printf("introducir el precio: \n");
     
-    scanf("%d",&cantitad);
+    scanf(" %d ",& cantitad);
     
-	if(cantitad > 50)
-	
-	cantitad=plato*75;
-	{	
-	
-	 	printf("los plato de la opcio 100 es: %d.\n", cantitad);
-	 	
+	if(50 < cantitad)
+	{
+		cantitad=plato*75;
+		printf("los plato de la opcio 100 es: %d.\n", cantitad);
 	}
-	
+	else
+		printf("No el precio no es suficiente");
+
 	return 0;
 	
 }
