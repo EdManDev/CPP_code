053ca51189aff962871bfcacaae26e36d267bb9c
edman manigat <e.manigat@gmail.com>
2018-11-23 09:45:22 -0400

    Update plato3.cpp

--- plato3.cpp
+++ plato3.cpp
@@ -6,49 +6,58 @@ using namespace std;
 
 int main()
 {
-    int opt, cantitad_platos, res ;
+    int opt;
+    int cantitad_platos;
+    int res;
     
-    printf("BIENVENIDO A ESTE PROGRAMA: \n\n");
-    printf("Opciones de tipos de plato: \n\n\n");
+    printf("BIENVENIDO  EN EL MEJOR RESTORANTE DEL MUNDO: \n\n");
+    printf("Opciones de tipos de plato: \n\n");
     
-    printf("\n  opcion 1 = plato del Dia: \n");
-    printf("  opcion 2 = plato Medio: \n");
-    printf("  opcion 3 = plato fuerte: \n\n\n");
+    printf("\n  opcion 1 = [Plato Del    Dia]: \n");
+    printf("  opcion 2 = [Plato Del  Medio]: \n");
+    printf("  opcion 3 = [Plato Del Fuerte]: \n\n\n");
     
-    printf("Introducir la opcion del tipo de plato que prefieres: \n");
+    printf("Introducir la opcion del tipo de plato que prefieres:\n\n\n");
     
     scanf("%d",& opt);
     
     switch(opt)
     {
         case 1:
-            printf("\n Ahora seleciona en la lista de plato del Dia:\n");
+        {
+            printf("\nAhora Introducir un precio, lo que tu quiere  [Plato Del Dia]:\n");
 
-            printf("\n    la cantidad de platos menor o igual a 50 el precio es  100\n");
-            printf("    la cantidad de platos mayor a 50 y menor a 101 el precio es  75\n");
-            printf("    la cantidad de platos mayor a 100 el precio es  50\n");
+            printf("\n    -la cantidad de platos: menor o igual a 50 el precio es       = 100 DOP\n");
+            printf("    -la cantidad de platos: mayor a 50 y menor a 101 el precio es = 75 DOP\n");
+            printf("    -la cantidad de platos: mayor a 100 el precio es              = 50 DOP\n");
             
             printf("\nIntroduzca la Cantidad de platos a comprar\n\n\n");
             
             scanf("%d",& cantitad_platos);
-            if(cantitad_platos<=50)
+            if(cantitad_platos<=100)
             {
-                res=cantitad_platos*100;
+                res = cantitad_platos - 100;
                 printf("\nEl precio por plato es: 100 pesos\n");
-                printf("El precio total a pagar es: %d\n\n",res);
+                printf("Lo Que Te Falta es: %d\n\n",res);
+            }
+            else{
+                printf("Ok Gracias por su compra  \n");
             }
             break;
-            
+        }   
         case 2:
+        {
             printf("empty");
-            break;
-            
+            break;        
+        }   
         case 3:
+        {
             printf("empty");
-            break;
+            break;       
+        }
             
         default:
-            printf(" Nigun opciones han sido selectionado ");
+            printf(" Nigun opciones han sido selectionado! \n");
     }
     return 0;
 }
