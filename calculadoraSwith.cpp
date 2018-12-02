655ab4bfd05db09cfb3e88947ab613ff7c3eb2c1
edman manigat <e.manigat@gmail.com>
2018-12-02 08:09:41 -0400

    editor

--- calculadoraSwith.cpp
+++ calculadoraSwith.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include<iostream>
 #include<math.h>
-#include<conio.h>
 
 using namespace std;
 
--- calculadoraWhile.cpp
+++ calculadoraWhile.cpp
@@ -1,22 +1,58 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main()
 {
-    int number;
-    long long factorial;
-
-    printf("Enter an integer: ");
-    scanf("%d",&number);
-
-    factorial = 1;
-
-    // loop terminates when number is less than or equal to 0
-    while (number > 0)
+    char signo;
+    double num1, num2, counter = 0;
+    do
     {
-        factorial *= number;  // factorial = factorial*number;
-        --number;
-    }
+        cout << "Engrese Un Signo De Operador como: + o - o * o / : +, -, *,: ";
+        cin >> signo;
+        if (signo != 'Q')
+        {
+            cout << "Engrese Dos Numero: ";
+            cin >> num1;
+            cin >> num2;
+            cin.ignore();
+            if (num2 <= 0)
+            {
+                cout << "ERROR! DIVIDE BY ZERO!! ";
+                cout << endl;
+            }
+            else
+            {
+                if (signo == '+')
+                {
+                    num1 += num2;
+                }
+                else
+                {
+                    if (signo == '-')
+                    {
+                        num1 -= num2;
+                    }
+                    else
+                    {
+                        if (signo == '*')
+                        {
+                            num1 *= num2;
+                        }
+                        else
+                        {
+                            if ((signo == '/') && (num2 != 0))
+                            {
+                                num1 /= num2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cout << "the result is " << num1 << endl;
 
-    printf("Factorial= %lld", factorial);
-
-    return 0;
+    } while (signo != 'Q');
+    return (0);
 }
