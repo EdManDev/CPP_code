c5f4a5dfe7fa0041477043f609f46258e48ef1b9
EdManDev <ed.manigat@gmail.com>
2018-12-06 15:59:10 -0500

    Add files via upload

--- plato3.cpp
+++ plato3.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include<math.h>
+#include<conio.h>
 #include<stdio.h>
-
-using namespace std;
 
 int main()
 {
@@ -10,7 +9,7 @@ int main()
     int cantitad_platos;
     int res;
     
-    printf("BIENVENIDO  EN EL MEJOR RESTORANTE DEL MUNDO: \n\n");
+    printf("BIENVENIDO  EN EL MEJOR RESTORANTE DEL MUNDO : \n\n");
     printf("Opciones de tipos de plato: \n\n");
     
     printf("\n  opcion 1 = [Plato Del    Dia]: \n");
@@ -34,7 +33,7 @@ int main()
             printf("\nIntroduzca la Cantidad de platos a comprar\n\n\n");
             
             scanf("%d",& cantitad_platos);
-            if(cantitad_platos<=100)
+            if(cantitad_platos < 100)
             {
                 res = cantitad_platos - 100;
                 printf("\nEl precio por plato es: 100 pesos\n");
@@ -49,15 +48,17 @@ int main()
         {
             printf("\nAhora Introducir un precio, lo que tu quiere  [Plato Del  Medio]:\n\n");
 
+            // printf("\n    -la cantidad de platos: menor o igual a 50 el precio es       = 100 DOP\n");
             printf("    -la cantidad de platos: mayor a 50 y menor a 101 el precio es = 75 DOP\n");
+            printf("    -la cantidad de platos: mayor a 100 el precio es              = 50 DOP\n");
             
             printf("\nIntroduzca la Cantidad de platos a comprar\n\n\n");
             
             scanf("%d",& cantitad_platos);
-            if(cantitad_platos<=200)
+            if(cantitad_platos < 200)
             {
                 res = cantitad_platos - 200;
-                printf("\nEl precio por plato es: 100 pesos\n");
+                printf("\nEl precio por plato es: 200 pesos\n");
                 printf("Lo Que Te Falta es: %d\n\n",res);
             }
             else{
@@ -68,14 +69,18 @@ int main()
         case 3:
         {
             printf("\nAhora Introducir un precio, lo que tu quiere  [Plato Del Fuerte]:\n\n");
-            
+
+            // printf("\n    -la cantidad de platos: menor o igual a 50 el precio es       = 100 DOP\n");
+            // //printf("    -la cantidad de platos: mayor a 50 y menor a 101 el precio es = 75 DOP\n");
+            printf("    -la cantidad de platos: mayor a 100 el precio es              = 50 DOP\n");
+
             printf("\nIntroduzca la Cantidad de platos a comprar\n\n\n");
             
             scanf("%d",& cantitad_platos);
-            if(cantitad_platos<=500)
+            if(cantitad_platos < 500)
             {
                 res = cantitad_platos - 500;
-                printf("\nEl precio por plato es: 100 pesos\n");
+                printf("\nEl precio por plato es: 500 pesos\n");
                 printf("Lo Que Te Falta es: %d\n\n",res);
             }
             else{
